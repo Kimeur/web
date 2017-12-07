26ce5c66d8485b34792354e94600165ce0c47233
Mar1gazouzi <gazouzimarouane@gmail.com>
2017-12-07 23:11:18 +0100

    " data into graph"

--- src/main/resources/static/javascript/employee.js
+++ src/main/resources/static/javascript/employee.js
@@ -1,6 +1,21 @@
 console.log("kkd");
-console.log(tab);
 window.onload = function() {
+	var salaries_labels= [];
+	var salaries_values= []
+	for (var key in tab) {
+		var salary = tab[key].salary;
+		if (salaries_labels.includes(salary) == 1)
+			{
+			salaries_values[salaries_labels.indexOf(salary)]++;
+			}
+		else
+			{
+			salaries_labels.push(salary);
+			salaries_values[salaries_labels.indexOf(salary)]=1;
+			}
+		};
+	console.log(salaries_labels);
+	console.log(salaries_values);
 	var ctx = document.getElementById('myChart').getContext('2d');
 	
 	var chart = new Chart(ctx, {
@@ -9,12 +24,11 @@ window.onload = function() {
 
 	    // The data for our dataset
 	    data: {
-	        labels: ["January", "February", "March", "April", "May", "June", "July"],
+	        labels: salaries_labels,
 	        datasets: [{
-	            label: "My First dataset",
-	            backgroundColor: 'rgb(255, 99, 132)',
-	            borderColor: 'rgb(255, 99, 132)',
-	            data: [0, 10, 5, 2, 20, 30, 45],
+	            label: "Salaries",
+	            data: salaries_values,
+	            
 	        }]
 	    },
 
